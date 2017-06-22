a3d511d14a1b8275fb0cdf5af901b7ac6a85f184
Ramjeevan Patha <ramjeevan2006@gmail.com>
2017-06-22 17:06:03 +0530

    Add files via upload
    Data base(MySQL) scripts to create DB model for this project

---
+++ rv_subscriptions.sql
@@ -0,0 +1,46 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.10-log : Database - rv_subscription
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rv_subscription` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `rv_subscription`;
+
+/*Table structure for table `customer_details` */
+
+DROP TABLE IF EXISTS `customer_details`;
+
+CREATE TABLE `customer_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `email_id` varchar(100) DEFAULT NULL,
+  `organization_name` varchar(100) DEFAULT NULL,
+  `phone_number` varchar(10) DEFAULT NULL,
+  `activation_key` varchar(200) DEFAULT NULL,
+  `ip_address` varchar(50) DEFAULT NULL,
+  `registration_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `activation_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expiry_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `num_of_days` int(11) DEFAULT NULL,
+  `is_validated` tinyint(1) DEFAULT '0',
+  `is_active` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `customer_details` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
